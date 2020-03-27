634da197b247b2c2ea1b8c8a405d577991c9fbbc
cheungeric0308 <49165953+cheungeric0308@users.noreply.github.com>
2020-03-27 15:33:11 +0800

    Add index.php and login.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+<title>index</title>
+</head>
+<body>
+
+<form action="login.php" method="post">
+	Enter your username: <input name="name" type="text">
+	<input type="submit">
+</form>
+
+</body>
+</html>
