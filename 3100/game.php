0a642b3edb20e840bee704b0558cf1828005ca16
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-03-27 21:00:57 +0800

    Update game.php

--- 3100/game.php
+++ 3100/game.php
@@ -1,7 +1,12 @@
 <html>
 <head><title>game</title></head>
 <body>
-
-
+	<div id="frame2" style="text-align:center;height:50px;">
+		<form method="post">
+			<input type="button" value="Play" onClick="this.form.action='play.php';this.form.submit();">
+			<input type="button" value="Join Room" onClick="this.form.action='join.php';this.form.submit();">
+			<input type="button" value="Play Randomly" onClick="this.form.action='random.php';this.form.submit();">
+		</form>
+	</div>
 </body>
 </html>
