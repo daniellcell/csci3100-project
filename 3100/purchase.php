73b67108a81e34c6365c9d1fa9d2c1f16aed778d
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-03-27 21:48:10 +0800

    Update purchase.php

--- 3100/purchase.php
+++ 3100/purchase.php
@@ -4,6 +4,7 @@
 
 <form action="shoppingcart.php" method="post">
     <input type="submit" id="button" value="Add to cart">
+    <input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">
 </form>
 
 </body>
