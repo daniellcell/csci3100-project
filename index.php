1dd82fc2c45a504856debbf2c9c01b487e1a461d
cheungeric0308 <49165953+cheungeric0308@users.noreply.github.com>
2020-03-27 15:49:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,19 @@
 </head>
 <body>
 
-<form action="login.php" method="post">
-	Enter your username: <input name="name" type="text">
-	<input type="submit">
-</form>
+	<div id="frm">
+		<form action="login.php" method="post">
+			<p>
+				Username: <input name="name" "id="name" type="text">
+			</p>
+			<p>
+				Password: <input name="id" id="password" type="password">
+			</p>
+			<p>
+				<input type="submit" id="btn" value="Login">
+			</p>
+		</form>
+	</div>
 
 </body>
 </html>
