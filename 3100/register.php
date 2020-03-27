3bba14e186b93a54a33ed29c53cdccbebfe7a8e0
Iris Chan <38353659+ic17@users.noreply.github.com>
2020-03-27 21:04:21 +0800

    Create register.php

---
+++ 3100/register.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+<title>Register</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+	<div id="frame1">
+		<form action="process_r.php" method="post">
+			<p>
+				Username: <input name="username" id="username" type="text">
+			</p>
+			<p>
+				Password: <input name="password" id="password" type="password">
+			</p>
+			<p>
+				Re-enter Password: <input name="password2" id="password2" type="password">
+			</p>
+			<p>
+				<input type="submit" id="button" value="Register">
+			</p>
+		</form>
+	</div>
+</body>
+</html>
