52fc930449cefd1f78c4bf823984e67527015f90
daniellcell <49424544+daniellcell@users.noreply.github.com>
2020-03-27 18:28:55 +0900

    Add files via upload

---
+++ purchase.php
@@ -0,0 +1,10 @@
+<html>
+<head><title>login</title></head>
+<body>
+
+<form action="shoppingcart.php" method="post">
+    <input type="submit" id="button" value="Add to cart">
+</form>
+
+</body>
+</html>
---
+++ shoppingcart.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>login</title></head>
+<body>
+
+<form action="purchase.php" method="post">
+    <input type="submit" id="button" value="Continue to shopping">
+</form>
+
+<form action="payment.php" method="post">
+    <input type="submit" id="button" value="Payment">
+</form>
+
+</body>
+</html>
