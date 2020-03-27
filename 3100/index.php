dd3cdf746c637b8c7d3ad41654d795efbbf22a8c
Iris Chan <38353659+ic17@users.noreply.github.com>
2020-03-27 23:21:17 +0800

    Update index.php

--- 3100/index.php
+++ 3100/index.php
@@ -1,3 +1,13 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION['loginuser'])) {
+	echo "Hello, ".$_SESSION['loginuser'];
+}
+
+?>
+
 <html>
 <head>
 <title>Main Page</title>
@@ -9,6 +19,7 @@
 			<input type="button" value="login" onClick="this.form.action='login.php';this.form.submit();">
 			<input type="button" value="game" onClick="this.form.action='game.php';this.form.submit();">
 			<input type="button" value="purchase" onClick="this.form.action='purchase.php';this.form.submit();">
+			<input type="button" value="logout" onClick="this.form.action='logout.php';this.form.submit();">
 		</form>
 	</div>
 		
