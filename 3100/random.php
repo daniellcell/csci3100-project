d6eec5dbdf6ffd637ffde32f4ad78183d5bfaa0d
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-03-27 23:07:57 +0800

    Update random.php

--- 3100/random.php
+++ 3100/random.php
@@ -2,5 +2,6 @@
 <head><title>Random</title></head>
 <body>
   Please choose which game you would like to play.<br>
+  <input type="button" value="Return" onClick="this.form.action='game.php';this.form.submit();">
 </body>
 </html>
