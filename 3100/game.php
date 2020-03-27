c2446d4d8bcfa103d2a1d3d0543adbff102bf065
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-03-27 21:12:18 +0800

    Update game.php

--- 3100/game.php
+++ 3100/game.php
@@ -6,6 +6,7 @@
 			<input type="button" value="Play" onClick="this.form.action='play.php';this.form.submit();">
 			<input type="button" value="Join Room" onClick="this.form.action='join.php';this.form.submit();">
 			<input type="button" value="Play Randomly" onClick="this.form.action='random.php';this.form.submit();">
+			<input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">
 		</form>
 	</div>
 </body>
