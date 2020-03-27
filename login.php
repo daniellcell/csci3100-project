60eeca40dfbcb0a597c213b144601e2ec4fe5478
daniellcell <49424544+daniellcell@users.noreply.github.com>
2020-03-27 17:50:55 +0900

    Update login.php

--- login.php
+++ login.php
@@ -4,11 +4,19 @@
 
 
 <?php
-
-	$name = $_POST["name"];
-	echo "Hello, ", $name
-	
+    
+    $name = $_POST["name"];
+    echo "Hello, ", $name;
+    echo nl2br("\n");
+    echo "You have successfully logged in to the system."
+	    
 ?>
-
+	
+<form action="index.php" method="post">
+	
+	<input type="submit" id="button" value="Return">
+	
+</form>
+	
 </body>
 </html>
