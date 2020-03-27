22a4b0d96cd85f1bcec7dd899272369f8a3bcad4
Iris Chan <38353659+ic17@users.noreply.github.com>
2020-03-27 20:38:32 +0800

    Update payment.php

--- 3100/payment.php
+++ 3100/payment.php
@@ -4,5 +4,15 @@
 
 <!--The page for payment. -->
 
+Payment successful.
+<br><br>
+
+<form action="index.php" method="post">
+    <input type="submit" id="button" value="Back to Main Page">
+</form>
+<form action="purchase.php" method="post">
+    <input type="submit" id="button" value="Purchase again">
+</form>
+
 </body>
 </html>
