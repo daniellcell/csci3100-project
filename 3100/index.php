8b2f976f770c6821787bd96a80e73483f171520b
daniellcell <49424544+daniellcell@users.noreply.github.com>
2020-04-22 01:53:16 +0900

    Update index.php

--- 3100/index.php
+++ 3100/index.php
@@ -1,27 +1,9 @@
 <?php
-
 session_start();
-
-if (isset($_SESSION['loginuser'])) {
-	echo "Hello, ".$_SESSION['loginuser'];
-}
-
+// Include functions and connect to the database using PDO MySQL
+include 'functions.php';
+// Page is set to home (home.php) by default, so when the visitor visits that will be the page they see.
+$page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
+// Include and show the requested page
+include $page . '.php';
 ?>
-
-<html>
-<head>
-<title>Main Page</title>
-<link href="style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-	<div id="frame2" style="text-align:center;height:50px;">
-		<form method="post">
-			<input type="button" value="login" onClick="this.form.action='login.php';this.form.submit();">
-			<input type="button" value="game" onClick="this.form.action='game.php';this.form.submit();">
-			<input type="button" value="purchase" onClick="this.form.action='purchase.php';this.form.submit();">
-			<input type="button" value="logout" onClick="this.form.action='logout.php';this.form.submit();">
-			<input type="button" value="chat" onClick="this.form.action='chat.html';this.form.submit();">
-		</form>
-	</div>
-</body>
-</html>
