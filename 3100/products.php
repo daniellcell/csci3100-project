aab4ecce8a054c8b16acb05ee40f70b3370d4d9e
daniellcell <49424544+daniellcell@users.noreply.github.com>
2020-04-22 02:12:10 +0900

    Add files via upload

--- 3100/products.php
+++ 3100/products.php
@@ -1,12 +1,11 @@
 <?php
-    include 'functions.php';
-    $pdo = pdo_connect_shoppingcart();
+$pdo = pdo_connect_shoppingcart();
 // The amounts of products to show on each page
 $num_products_on_each_page = 4;
 // The current page, in the URL this will appear as index.php?page=products&p=1, index.php?page=products&p=2, etc...
 $current_page = isset($_GET['p']) && is_numeric($_GET['p']) ? (int)$_GET['p'] : 1;
 // Select products ordered by the date added
-$stmt = $pdo->prepare('SELECT * FROM products ORDER BY date_added DESC LIMIT ?,?');
+$stmt = $pdo->prepare('SELECT * FROM products');
 // bindValue will allow us to use integer in the SQL statement, we need to use for LIMIT
 $stmt->bindValue(1, ($current_page - 1) * $num_products_on_each_page, PDO::PARAM_INT);
 $stmt->bindValue(2, $num_products_on_each_page, PDO::PARAM_INT);
