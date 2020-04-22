6dd2efc7726a5f860b4749716dab1a1432568f2d
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-04-22 15:58:26 +0800

    Update game.php
    added header and footer

--- 3100/game.php
+++ 3100/game.php
@@ -1,13 +1,12 @@
-<html>
-<head><title>game</title></head>
-<body>
-	<div id="frame2" style="text-align:center;height:50px;">
+<?=template_header('Game')?>
+
+<div id="frame2" style="text-align:center;height:50px;">
 		<form method="post">
 			<input type="button" value="Play" onClick="this.form.action='play.php';this.form.submit();">
 			<input type="button" value="Join Room" onClick="this.form.action='join.php';this.form.submit();">
 			<input type="button" value="Play Randomly" onClick="this.form.action='random.php';this.form.submit();">
 			<input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">
 		</form>
-	</div>
-</body>
-</html>
+</div>
+
+<?=template_footer()?>
