68440216d96f646d92551bad3f54877160bc91b6
cheungeric0308 <49165953+cheungeric0308@users.noreply.github.com>
2020-04-22 21:56:52 +0800

    Update home.php

--- 3100/home.php
+++ 3100/home.php
@@ -2,9 +2,9 @@
 <?php
 
 if (isset($_SESSION['loginuser'])) {
-	echo "Hello, ".$_SESSION['loginuser'];
+	echo "Hello, ".$_SESSION['loginuser']."! ";
 }
-    echo "! Welcome to Board Game Platform.";
+    echo "Welcome to Board Game Platform.";
 ?>
 
 <?=template_footer()?>
