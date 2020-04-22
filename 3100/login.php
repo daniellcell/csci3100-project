546e650429466c0b330193945544fec1120efce6
cheungeric0308 <49165953+cheungeric0308@users.noreply.github.com>
2020-04-22 18:20:22 +0800

    Add files via upload

--- 3100/login.php
+++ 3100/login.php
@@ -6,27 +6,18 @@
 <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-
-	<div id="frame1">
-		<form action="process.php" method="post">
-			<p>
-				Username: <input name="username" id="username" type="text">
-			</p>
-			<p>
-				Password: <input name="password" id="password" type="password">
-			</p>
-			<p>
-				<input type="submit" id="button" value="Login">
-				<input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">
-			</p>
-			<p>
-				Do not have an account? 
-				<form method="post">
+	<form class="box" action="process.php" method="post">
+	  <h1>Board Game Platform</h1>
+		  <input type="text" name="username" placeholder="Username">
+		  <input type="password" name="password" placeholder="Password">
+		  <input type="submit" name="" value="Login">
+		  <input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">
+			Do not have an account? 
+			<form method="post">
 					<input type="button" value="Register" onClick="this.form.action='register.php';this.form.submit();">
-				</form>
-			</p>
-		</form>
-	</div>
+			</form>
+	</form>
+
 </body>
 </html>
 <?=template_footer()?>
