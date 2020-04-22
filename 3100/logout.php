6c9114ad67e3fa2fa6dd4d3f17b4b7d566f5f04c
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-04-22 15:57:59 +0800

    Update logout.php
    added header and footer

--- 3100/logout.php
+++ 3100/logout.php
@@ -1,6 +1,5 @@
+<?=template_header('Logout')?>
 <?php
-
-session_start();
 
 	if (isset($_SESSION['loginuser'])) {
 		echo "You have logged out successfully.";
@@ -13,3 +12,5 @@ session_start();
 		echo '<form action="index.php" method="post"> <input type="submit" id="button" value="Return to Index"> </form>';
 	}
 ?>
+
+<?=template_footer()?>
