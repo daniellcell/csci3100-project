19dc50e142e899ca275442b8e07382cea9b0d614
Iris Chan <38353659+ic17@users.noreply.github.com>
2020-04-22 22:49:18 +0800

    Update Play.php

--- 3100/Play.php
+++ 3100/Play.php
@@ -8,6 +8,7 @@
 	<div id="play">
 		<form method = "post">
 			<p>
+			<input type="button" value="Matching" onClick="this.form.action='game_matching.php';this.form.submit();">
 			<input type="button" value="Leave" onClick="this.form.action='index.php';this.form.submit();">
 			</p>
 		</form>
