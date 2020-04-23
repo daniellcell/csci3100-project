34869a90b95778fe53e09a8b08b11e94e7b28164
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-04-23 15:19:55 +0800

    Update game.php

--- 3100/game.php
+++ 3100/game.php
@@ -1,18 +1,17 @@
 <?=template_header('Game')?>
 <?php
+	include 'functions.php';
+
 	if (!isset($_SESSION)) {
 		session_start();
 	}	
 ?>
 
-<div id="frame1" style="text-align:center; vertical-align: middle; height:30px;">
-		<form method="post">
-			<input type="button" value="Play with AI" onClick="this.form.action='play.php';this.form.submit();">
-			<!--<input type="button" value="Join Room" onClick="this.form.action='join.php';this.form.submit();"-->
-			<!--<input type="button" value="Play Randomly" onClick="this.form.action='random.php';this.form.submit();"-->
-			<input type="button" value="Rank" onClick="this.form.action='rank.php';this.form.submit();">
-			<input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">
-		</form>
-</div>
+<form class="box" method="post">
+	<input type="button" value="Play with AI" onClick="this.form.action='play.php';this.form.submit();">
+	<!--<input type="button" value="Join Room" onClick="this.form.action='join.php';this.form.submit();"-->
+	<!--<input type="button" value="Play Randomly" onClick="this.form.action='random.php';this.form.submit();"-->
+	<!--<input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">-->
+</form>
 
 <?=template_footer()?>
