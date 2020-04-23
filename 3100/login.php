3efbde390eab116c6d44451b991680beb6d18d5e
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-04-23 15:22:38 +0800

    Update login.php

--- 3100/login.php
+++ 3100/login.php
@@ -1,23 +1,13 @@
 <?=template_header('Login')?>
-<html>
-
-<head>
-<title>login</title>
-<link href="style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-	<form class="box" action="process.php" method="post">
-	  <h1>Board Game Platform</h1>
-		  <input type="text" name="username" placeholder="Username">
-		  <input type="password" name="password" placeholder="Password">
-		  <input type="submit" name="" value="Login">
-		  <input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">
-			Do not have an account? 
-			<form method="post">
-					<input type="button" value="Register" onClick="this.form.action='index.php?page=register';this.form.submit();">
-			</form>
+<form class="box" action="process.php" method="post">
+	<h1>Login</h1>
+	<input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <input type="submit" name="" value="Login">
+    <input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">
+	Don't have an account? 
+	<form method="post">
+		<input type="button" value="Register" onClick="this.form.action='index.php?page=register';this.form.submit();">
 	</form>
-
-</body>
-</html>
+</form>
 <?=template_footer()?>
