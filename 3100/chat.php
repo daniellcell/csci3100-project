5bab2c2018e21a2b585352f24cb6fef6b0b8ef95
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-04-23 22:49:37 +0800

    Update chat.php

--- 3100/chat.php
+++ 3100/chat.php
@@ -1,3 +1,5 @@
+<?=template_header('Chatroom')?>
+<?=template_footer()?>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,14 +12,23 @@
     body {
       padding: 0;
       margin: 0;
-      display: flex;
       justify-content: center;
     }
+	h1{
+		display: block;
+		font-weight: bold;
+		margin: 0;
+		padding: 20px 0;
+		font-size: 24px;
+		text-align: center;
+		width: 100%;
+	}
     /*message output area */
     #message-container {
+	  background-color: rgba(202, 214, 240, 0.6);
       width: 90%;
       max-width: 600px;
-      height: 750px;
+      height: 600px;
       overflow: auto;
       position: absolute;
       top: 50%;
@@ -25,28 +36,29 @@
       transform: translate(-50%,-55%);
       text-align: center;
       border-radius: 30px;
-      border: 2px solid MidnightBlue;
     }
 
     #message-container div {
+	  background-color: rgba(202, 214, 240, 0.6);
       padding: 5px;
       word-wrap: break-word;
-      color: Black;
+	  color: #384051;
     }
 
     #message-container div:nth-child(2n) {
       word-wrap: break-word;
-      color: MediumSpringGreen;
+      color: #394253;
     }
 
     /*message input + send button area */
     #send-container {
       position: fixed;
-      padding-bottom: 0px;
+      padding-bottom: 100px;
       bottom: 0;
       max-width: 600px;
       width: 80%;
       display: flex;
+	  left: 34.5%;
     }
 
     #message-input {
@@ -56,34 +68,35 @@
       display: block;
       margin: 20px auto;
       text-align: center;
-      border: 2px solid black;
+	  background-color: rgba(57, 66, 83, 0.6);	  
+	  color: #E8ECF4;
       padding: 14px 10px;
       width: 200px;
       outline: none;
-      color: black;	
       border-radius: 24px;
       transition: 0.25s;
     }
 
     #message-input:focus{
-      border-color: Chartreuse;
+	  background-color: rgba(57, 66, 83, 0.6);	  
     }
+	
     #send-button {
       border:0;
-      background: crimson;
+	  background-color: rgba(202, 214, 240, 0.6);
+	  color: #384051;
       display: block;
       margin: auto;
       text-align: center;
-      border: 2px solid DarkBlue;
       padding: 14px 20px;
       outline: none;
-      color: black;
       border-radius: 24px;
       transition: 0.25s;
       cursor: pointer;
     }
     #send-button:hover{
-      background: CornflowerBlue;
+	  background-color: rgba(57, 66, 83, 0.6);	  
+	  color: #E8ECF4;
     }
 
     /* scroll bar */
@@ -92,22 +105,23 @@
     }
 
     ::-webkit-scrollbar-track {
-      box-shadow: inset 0 0 5px grey; 
+      box-shadow: inset 0 0 5px rgba(202, 214, 240, 0.6); 
       border-radius: 24px;
     }
     
     ::-webkit-scrollbar-thumb {
-      background: grey; 
+      
+	  background: rgba(57, 66, 83, 0.6); 
       border-radius: 24px;
     }
 
     ::-webkit-scrollbar-thumb:hover {
-      background: SlateBlue; 
+      background: rgba(202, 214, 240, 0.6); 
     }
   </style>
   </head>
   <body>
-    <h1>Board Game Platform Chatroom</h1>
+    <h1>Chatroom</h1>
     <div id="message-container"></div>
     <form id="send-container">
       <input type="text" id="message-input" placeholder="Enter your message here">
