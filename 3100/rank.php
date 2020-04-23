df95859b4432d27a0bdd8efd4f8d531d15aa6a0c
Iris Chan <38353659+ic17@users.noreply.github.com>
2020-04-23 14:51:15 +0800

    Add files via upload

--- 3100/rank.php
+++ 3100/rank.php
@@ -9,7 +9,66 @@
 <?=template_header('Rank')?>
 
 <button onclick="window.location='index.php?page=game'">Return</button>
-<h1>Leaderboard</h1>
+<h1 style="font-size:35px;">Leaderboard</h1>
+<br>
+
+<style>
+table { 
+	text-align: center;
+	vertical-align: middle;
+	margin: auto;
+}
+</style>
+
+<table id="matching">
+	<caption>Matching</caption>
+	<tr>
+		<th>Rank</th>
+		<th>Username</th>
+		<th>Flips</th>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>3</td>
+		<td></td>
+		<td></td>
+	</tr>
+</table>
+
+<br><br>
+<table id="chess">
+	<caption>Connect Four</caption>
+	<tr>
+		<th>Rank</th>
+		<th>Username</th>
+		<th>Win %</th>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>3</td>
+		<td></td>
+		<td></td>
+	</tr>
+</table>
+
 
 <?php
 
