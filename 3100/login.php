42ff650370465ce4d8d1ba665b1e4b487fe0e150
cheungeric0308 <49165953+cheungeric0308@users.noreply.github.com>
2020-04-23 18:55:02 +0800

    Update login.php

--- 3100/login.php
+++ 3100/login.php
@@ -4,7 +4,6 @@
 	<input type="text" name="username" placeholder="Username">
     <input type="password" name="password" placeholder="Password">
     <input type="submit" name="" value="Login">
-    <input type="button" value="Return" onClick="this.form.action='index.php';this.form.submit();">
 	Don't have an account? 
 	<form method="post">
 		<input type="button" value="Register" onClick="this.form.action='index.php?page=register';this.form.submit();">
