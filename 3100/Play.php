111d841c9df98709730d8bd18884b54e41c74fa0
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-04-23 15:20:42 +0800

    Update Play.php

--- 3100/Play.php
+++ 3100/Play.php
@@ -8,27 +8,13 @@
 
 <?=template_header('Play')?>
 
-<html>
-<head>
-<title>Play</title>
-<link href="style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-
-	<div style="text-align:center; vertical-align: middle;">
-	Please choose game that you want to play with computer:)
-	<div id="play">
-		<form method = "post">
-			<p>
-			<input type="button" value="Matching" onClick="this.form.action='game_matching.php';this.form.submit();">
-			<input type="button" value="Chess" onClick="this.form.action='game_chess.php';this.form.submit();">
-			<input type="button" value="Leave" onClick="this.form.action='index.php';this.form.submit();">
-			</p>
-		</form>
-	</div>
-	</div>
-		
-</body>
-</html>
+<form class="box" method = "post">
+	<h2>Please choose game that you want to play with computer:)<br></h2>
+	<p>
+		<input type="button" value="Matching" onClick="this.form.action='game_matching.php';this.form.submit();">
+		<input type="button" value="Chess" onClick="this.form.action='game_chess.php';this.form.submit();">
+		<input type="button" value="Leave" onClick="this.form.action='index.php';this.form.submit();">
+	</p>
+</form>
 
 <?=template_footer()?>
