be23d4da74c630d9517b1000737c7d94c2129565
11551107C1 <49342296+11551107C1@users.noreply.github.com>
2020-04-23 20:48:18 +0800

    Update game_matching.php

--- 3100/game_matching.php
+++ 3100/game_matching.php
@@ -17,6 +17,7 @@
 
 <body>
 	<h3 id = "det" style="text-align:center">
+		<br><br><br>
 		This is the easy version of Concentration (card matching) game.<br>
 		How to win: match all the pairs of cards.<br>
 		Less number of flips is better.
