9b54b9a2cedfeb3a101d531a50dea7adb9f4e857
cheungeric0308 <49165953+cheungeric0308@users.noreply.github.com>
2020-05-15 22:03:09 +0800

    Update script.js

--- 3100/script.js
+++ 3100/script.js
@@ -3,22 +3,27 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
+// request the user to enter the name he/she wants to display
 const name = prompt('Enter the name you want to display in chatroom:')
 appendMessage(`${name} (You) connected to the chatroom`)
 socket.emit('new-user', name)
 
+// display other user's message
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
 })
 
+// display who connected
 socket.on('user-connected', name => {
   appendMessage(`${name} connected to the chatroom`)
 })
 
+// display who disconnected
 socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected from the chatroom`)
 })
 
+// display the current user's message
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
@@ -27,6 +32,7 @@ messageForm.addEventListener('submit', e => {
   messageInput.value = ''
 })
 
+// message function
 function appendMessage(message) {
   const messageElement = document.createElement('div')
   messageElement.innerText = message
