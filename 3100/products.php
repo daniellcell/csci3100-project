cdc02ff864311901c617e7146154c2d4c2f6ec8e
daniellcell <49424544+daniellcell@users.noreply.github.com>
2020-05-17 15:23:47 +0900

    Update products.php

--- 3100/products.php
+++ 3100/products.php
@@ -11,19 +11,16 @@ if (!isset($_SESSION['loginuser'])) {
 else{
     // The amounts of products to show on each page
     $num_products_on_each_page = 4;
-    // The current page, in the URL this will appear as index.php?page=products&p=1, index.php?page=products&p=2, etc...
     $current_page = isset($_GET['p']) && is_numeric($_GET['p']) ? (int)$_GET['p'] : 1;
-    // Select products ordered by the date added
     $pdo = pdo_connect_shoppingcart();
-    // Select all products
+    // Select products needed for the page
     $stmt = $pdo->prepare('SELECT * FROM products LIMIT ?,?');
     $stmt->bindValue(1, ($current_page - 1) * $num_products_on_each_page, PDO::PARAM_INT);
     $stmt->bindValue(2, $num_products_on_each_page, PDO::PARAM_INT);
     $stmt->execute();
-    // Fetch the products from the database and return the result as an Array
     $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // Get the total number of products
-        $total_products = $pdo->query('SELECT * FROM products')->rowCount();
+	// Get the total number of products
+    $total_products = $pdo->query('SELECT * FROM products')->rowCount();
 }
 ?>
 <style type="text/css">
