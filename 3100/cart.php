d5f67769eaa542523dae64d64734d8ce29504d04
daniellcell <49424544+daniellcell@users.noreply.github.com>
2020-05-17 15:21:54 +0900

    Update cart.php

--- 3100/cart.php
+++ 3100/cart.php
@@ -38,11 +38,10 @@
                     $_SESSION['cart'] = array($product_id => $quantity);
                 }
             }
-            // Prevent form resubmission...
             header('location: index.php?page=cart');
             exit;
         }
-        // Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
+        // Remove product from cart
         if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
             // Remove the product from the shopping cart
             unset($_SESSION['cart'][$_GET['remove']]);
@@ -65,7 +64,6 @@
                     }
                 }
             }
-            // Prevent form resubmission...
             header('location: index.php?page=cart');
             exit;
         }
@@ -81,13 +79,9 @@
         $subtotal = 0.00;
         // If there are products in cart
         if ($products_in_cart) {
-            // There are products in the cart so we need to select those products from the database
-            // Products in cart array to question mark string array, we need the SQL statement to include IN (?,?,?,...etc)
             $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
             $stmt = $pdo->prepare('SELECT * FROM products WHERE id IN (' . $array_to_question_marks . ')');
-            // We only need the array keys, not the values, the keys are the id's of the products
             $stmt->execute(array_keys($products_in_cart));
-            // Fetch the products from the database and return the result as an Array
             $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
             // Calculate the subtotal
             foreach ($products as $product) {
